355f37d51b477cee76035a7ea8ee51a5d252b11e
Bruno Helder <brunohrguedes@gmail.com>
2024-12-15 19:30:05 -0300

    feat: create dialog ui

---
+++ src/npc-dialog-ui.tsx
@@ -0,0 +1,13 @@
+import ReactEcs, { ReactEcsRenderer, UiEntity } from '@dcl/sdk/react-ecs'
+import { NpcUtilsUi } from 'dcl-npc-toolkit'
+
+const SceneOwnedUi = () => (
+  <UiEntity>
+    <NpcUtilsUi />
+    {/* rest of user defined UI */}
+  </UiEntity>
+)
+
+export function setupUi() {
+  ReactEcsRenderer.setUiRenderer(SceneOwnedUi)
+}

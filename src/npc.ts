d4f7e2fe027fbb7e34388088096c7e65303082b3
Bruno Helder <brunohrguedes@gmail.com>
2024-12-15 19:29:34 -0300

    feat: create myNPC

---
+++ src/npc.ts
@@ -0,0 +1,19 @@
+import * as npc from 'dcl-npc-toolkit'
+import { Vector3, Quaternion } from '@dcl/sdk/math'
+
+export let myNPC = npc.create(
+  // TransformType
+  {
+    position: Vector3.create(163.75, 18.0, 192.25),
+    rotation: Quaternion.Zero(),
+    scale: Vector3.create(1, 1, 1)
+  },
+  // NPCData Object
+  {
+    type: npc.NPCType.CUSTOM,
+    model: 'models/npc.glb',
+    onActivate: () => {
+      console.log('npc activated')
+    }
+  }
+)

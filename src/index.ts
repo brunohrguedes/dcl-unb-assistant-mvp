1165963c0cfa2b2dee17e33fd1dd6d87a9fa7822
Bruno Helder <brunohrguedes@gmail.com>
2024-12-15 13:33:24 -0300

    fix: remove broken code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-import { setupUi } from './ui'
-
 export function main() {
   //setupUi()
-  executeTask(async () => {
-    try {
-      let response = await fetch(callUrl, {
-        headers: { 'Content-Type': 'application/json' },
-        method: 'POST',
-        body: JSON.stringify(myBody)
-      })
-      let json = await response.json()
-      console.log(json)
-    } catch {
-      console.log('failed to reach URL')
-    }
-  })
 }

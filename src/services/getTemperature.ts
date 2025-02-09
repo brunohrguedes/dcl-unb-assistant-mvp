a5b2eb078881ce258f05e87d94a285b1063d8769
Bruno Helder <brunohrguedes@gmail.com>
2025-02-09 17:52:04 -0300

    refactor: move get weather to services folder

---
+++ src/services/getTemperature.ts
@@ -0,0 +1,12 @@
+export async function getTemperature() {
+  const response = await fetch(
+    'https://api.open-meteo.com/v1/forecast?latitude=-15.76&longitude=-47.87&current=temperature_2m'
+  )
+  const { current } = await response.json()
+
+  console.log('Temperatura atual: ', current.temperature_2m)
+
+  return current.temperature_2m
+}
+
+export const temp = await getTemperature()

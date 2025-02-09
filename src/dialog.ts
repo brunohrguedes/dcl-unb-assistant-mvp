cf5691709ae9d93f8edf14847d56db728fcc04d6
Bruno Helder <brunohrguedes@gmail.com>
2025-02-09 18:03:37 -0300

    fix: move get weather to a executeTask function instead

--- src/dialog.ts
+++ src/dialog.ts
@@ -1,5 +1,5 @@
+import { executeTask } from '@dcl/sdk/ecs'
 import { Dialog } from 'dcl-npc-toolkit'
-import { getTemperature } from './services/getTemperature'
 
 let temperature
 
@@ -22,7 +22,16 @@ export let WelcomeDialog: Dialog[] = [
         label: 'Sim!',
         goToDialog: 3,
         triggeredActions: async () => {
-          await getTemperature().then((temp) => (temperature = temp))
+          executeTask(async () => {
+            const response = await fetch(
+              'https://api.open-meteo.com/v1/forecast?latitude=-15.76&longitude=-47.87&current=temperature_2m'
+            )
+            const { current } = await response.json()
+
+            console.log('Temperatura atual: ', current.temperature_2m)
+
+            temperature = current.temperature_2m
+          })
         }
       }
     ]

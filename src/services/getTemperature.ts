e0a35991088723ddc85f94e496b1857005191db1
Bruno Helder <brunohrguedes@gmail.com>
2025-02-09 17:57:03 -0300

    fix: use execute task to run async get weather function

--- src/services/getTemperature.ts
+++ src/services/getTemperature.ts
@@ -1,12 +1,16 @@
+import { executeTask } from '@dcl/sdk/ecs'
+
 export async function getTemperature() {
-  const response = await fetch(
-    'https://api.open-meteo.com/v1/forecast?latitude=-15.76&longitude=-47.87&current=temperature_2m'
-  )
-  const { current } = await response.json()
+  executeTask(async () => {
+    const response = await fetch(
+      'https://api.open-meteo.com/v1/forecast?latitude=-15.76&longitude=-47.87&current=temperature_2m'
+    )
+    const { current } = await response.json()
 
-  console.log('Temperatura atual: ', current.temperature_2m)
+    console.log('Temperatura atual: ', current.temperature_2m)
 
-  return current.temperature_2m
+    return current.temperature_2m
+  })
 }
 
 export const temp = await getTemperature()
